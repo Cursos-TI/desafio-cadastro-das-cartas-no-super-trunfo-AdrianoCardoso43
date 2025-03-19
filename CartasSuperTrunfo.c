126bbacb5d61ca625a93a5223a4325c80cd189f7
AdrianoCardoso43 <adrianocardosodarosa@gmail.com>
2025-03-19 17:58:36 +0000

    corrigindo erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,7 +16,7 @@ int main(){
     printf("digite o numero de pontos turisticos: \n");
     scanf("%d", &pontoturistico);
 
-    printf("Total da populacao: %d - Total de pontos turisticos: %d\n55", populacao, pontoturistico);
+    printf("Total da populacao: %d - Total de pontos turisticos: %d\n", populacao, pontoturistico);
     printf("Total da area: %f - Total do Pib %f", area,pib);
 
 
